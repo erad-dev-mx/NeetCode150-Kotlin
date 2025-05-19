e83e9471d56d94f8c92e684dd2c9230e18b617fb
erad-dev-mx <erick_rr1@outlook.com>
2025-05-19 13:41:12 -0600

    LeetCode 242 solution

---
+++ src/leetCode242/Solution.kt
@@ -0,0 +1,44 @@
+package leetCode242
+
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+
+        val count = IntArray(26)
+
+        for (i in s.indices) {
+            count[s[i] - 'a']++
+            count[t[i] - 'a']--
+        }
+
+        for (i in count.indices) {
+            if (count[i] != 0) return false
+        }
+
+        return true
+    }
+}
+
+// Test cases
+fun main() {
+    val solution = Solution()
+
+    // Test case 1
+    val s1 = "anagram"
+    val t1 = "nagaram"
+    println(solution.isAnagram(s1, t1)) // Expected output: true
+
+    // Test case 2
+    val s2 = "rat"
+    val t2 = "car"
+    println(solution.isAnagram(s2, t2)) // Expected output: false
+
+    // Additional test cases
+    val s3 = "listen"
+    val t3 = "silent"
+    println(solution.isAnagram(s3, t3)) // Expected output: true
+
+    val s4 = "hello"
+    val t4 = "world"
+    println(solution.isAnagram(s4, t4)) // Expected output: false
+}
