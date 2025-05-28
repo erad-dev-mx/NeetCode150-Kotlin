dd409aaef21601115e8455429eccc2c8b64e5cc6
erad-dev-mx <erick_rr1@outlook.com>
2025-05-27 19:38:31 -0600

    LeetCode 238 test cases

--- src/leetCode238/Solution.kt
+++ src/leetCode238/Solution.kt
@@ -27,3 +27,20 @@ class Solution {
         return result
     }
 }
+
+// Test cases
+fun main() {
+    val solution = Solution()
+
+    val nums1 = intArrayOf(1, 2, 3, 4)
+    println(solution.productExceptSelf(nums1).contentToString()) // Output: [24, 12, 8, 6]
+
+    val nums2 = intArrayOf(-1, 1, 0, -3, 3)
+    println(solution.productExceptSelf(nums2).contentToString()) // Output: [0, 0, 9, 0, 0]
+
+    val nums3 = intArrayOf(1)
+    println(solution.productExceptSelf(nums3).contentToString()) // Output: [1]
+
+    val nums4 = intArrayOf(1, 2)
+    println(solution.productExceptSelf(nums4).contentToString()) // Output: [2, 1]
+}
