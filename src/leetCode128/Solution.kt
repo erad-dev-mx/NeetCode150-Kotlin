726fc58928f87e1b2ca706c762d6a2518dd3939b
erad-dev-mx <erick_rr1@outlook.com>
2025-06-10 19:54:49 -0600

    LeetCode 128 test cases

--- src/leetCode128/Solution.kt
+++ src/leetCode128/Solution.kt
@@ -24,3 +24,12 @@ class Solution {
         return longestSub
     }
 }
+
+// Test cases
+fun main() {
+    val solution = Solution()
+    println(solution.longestConsecutive(intArrayOf(100, 4, 200, 1, 3, 2))) // Output: 4
+    println(solution.longestConsecutive(intArrayOf(0, 3, 7, 2, 5, 8, 4, 6, 1))) // Output: 9
+    println(solution.longestConsecutive(intArrayOf())) // Output: 0
+    println(solution.longestConsecutive(intArrayOf(1))) // Output: 1
+}
