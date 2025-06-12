c095a00fb90f2b3cafca8dd725c52a264ec8fe67
erad-dev-mx <erick_rr1@outlook.com>
2025-06-11 19:00:21 -0600

    LeetCode 125 solution

---
+++ src/leetCode125/Solution.kt
@@ -0,0 +1,24 @@
+package leetCode125
+
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+        var left = 0
+        var right = s.length - 1
+
+        while (left < right) {
+            while (left < right && !s[left].isLetterOrDigit()) {
+                left++
+            }
+            while (left < right && !s[right].isLetterOrDigit()) {
+                right--
+            }
+
+            while (s[left].lowercaseChar() != s[right].lowercaseChar()) {
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
