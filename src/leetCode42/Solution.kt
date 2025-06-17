09176176d1cd5a35a34ea2b08c009d356a7e7fc4
erad-dev-mx <erick_rr1@outlook.com>
2025-06-16 20:29:23 -0600

    LeetCode 42 test cases

--- src/leetCode42/Solution.kt
+++ src/leetCode42/Solution.kt
@@ -7,7 +7,6 @@ package leetCode42
  * This approach uses a Two-Pointer technique
  * */
 
-
 class Solution {
     fun trap(height: IntArray): Int {
         var left = 0
@@ -35,4 +34,21 @@ class Solution {
 
         return total
     }
+}
+
+// Test cases
+fun main() {
+    val solution = Solution()
+
+    val heights1 = intArrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)
+    println("Trapped water ${solution.trap(heights1)}")
+
+    val heights2 = intArrayOf(4, 2, 0, 3, 2, 5)
+    println("Trapped water ${solution.trap(heights2)}")
+
+    val heights3 = intArrayOf(2, 0, 2)
+    println("Trapped water ${solution.trap(heights3)}")
+
+    val heights4 = intArrayOf(0, 0, 0, 0, 0)
+    println("Trapped water ${solution.trap(heights4)}")
 }
