7a0e2d77788816f9e3b14b3c465b5cf921ccfbe5
erad-dev-mx <erick_rr1@outlook.com>
2025-06-17 19:28:59 -0600

    LeetCode 121 comments

--- src/LeetCode121/Solution.kt
+++ src/LeetCode121/Solution.kt
@@ -1,5 +1,11 @@
 package LeetCode121
 
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ * */
+
 class Solution {
     fun maxProfit(prices: IntArray): Int {
         var min = prices[0]
