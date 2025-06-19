96e104f172f1c19323fc6b67b6c8904e40c27336
erad-dev-mx <erick_rr1@outlook.com>
2025-06-18 19:41:54 -0600

    LeetCode 424 solution

---
+++ src/leetCode424/Solution.kt
@@ -0,0 +1,25 @@
+package leetCode424
+
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        val occurrence = IntArray(26)
+        var left = 0
+        var ans = 0
+        var maxOccurrence = 0
+
+        for (right in s.indices) {
+            val charIndex = s[right] - 'A'
+            occurrence[charIndex]++
+            maxOccurrence = maxOf(maxOccurrence, occurrence[charIndex])
+
+            if (right - left + 1 - maxOccurrence > k) {
+                occurrence[s[left] - 'A']--
+                left++
+            }
+
+            ans = maxOf(ans, right - left + 1)
+        }
+
+        return ans
+    }
+}
