aa16a60a2e22978f109e8df7d28467033047fede
erad-dev-mx <erick_rr1@outlook.com>
2025-06-24 20:30:12 -0600

    LeetCode 76 solution

--- src/leetCode76/Solution.kt
+++ src/leetCode76/Solution.kt
@@ -2,6 +2,46 @@ package leetCode76
 
 class Solution {
     fun minWindow(s: String, t: String): String {
-        
+        if (s.isEmpty() || t.isEmpty() || s.length < t.length) return ""
+
+        val mapT = mutableMapOf<Char, Int>()
+
+        for (i in t.indices) {
+            mapT[t[i]] = mapT.getOrDefault(t[i], 0) + 1
+        }
+
+        var required = mapT.size
+        var left = 0
+        var right = 0
+        var create = 0
+        var ans = intArrayOf(-1, 0, 0)
+        var subStringMap = mutableMapOf<Char, Int>()
+
+        while (right < s.length) {
+            var c = s[right]
+            val count = subStringMap.getOrDefault(c, 0)
+            subStringMap[c] = count + 1
+
+            if (mapT.contains(c) && subStringMap[c] == mapT[c]) {
+                create++;
+            }
+
+            while (left <= right && required == create) {
+                c = s[left]
+                if (ans[0] == -1 || ans[0] >= right - left + 1) {
+                    ans[0] = right - left + 1
+                    ans[1] = left
+                    ans[2] = right
+                }
+                subStringMap[c] = subStringMap[c]!! - 1
+                if (mapT.containsKey(c) && subStringMap[c]!! < mapT[c]!!) {
+                    create--
+                }
+                left++
+            }
+            right++
+        }
+
+        return if (ans[0] == -1) "" else s.substring(ans[1], ans[2] + 1)
     }
 }
