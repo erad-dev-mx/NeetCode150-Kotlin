5891dd227c56d9d14c89ec0b17afd37703125e9c
erad-dev-mx <erick_rr1@outlook.com>
2025-06-25 19:56:17 -0600

    LeetCode 239 solution

---
+++ src/leetCode239/Solution.kt
@@ -0,0 +1,35 @@
+package leetCode239
+
+import java.util.Deque
+import java.util.LinkedList
+
+class Solution {
+    fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+        if (nums.isEmpty() || k <= 0) return intArrayOf()
+
+        val n = nums.size
+        val result = IntArray(n - k + 1)
+        val deque: Deque<Int> = LinkedList()
+
+        for (i in nums.indices) {
+            // Removing indices that are out of the current window
+            while (deque.isNotEmpty() && deque.peek() < i - k + 1) {
+                deque.poll()
+            }
+
+            // Removing indices whose corresponding values are less than nums[i]
+            while (deque.isNotEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast()
+            }
+
+            // Adding the current index to the deque
+            deque.offer(i)
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peek()]
+            }
+        }
+
+        return result
+    }
+}
