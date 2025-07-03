f8c6e28f45361344e09e42b928b7d2b367e46def
erad-dev-mx <erick_rr1@outlook.com>
2025-07-02 19:57:28 -0600

    LeetCode 150 solution

--- src/leetCode150/Solution.kt
+++ src/leetCode150/Solution.kt
@@ -2,6 +2,32 @@ package leetCode150
 
 class Solution {
     fun evalRPN(tokens: Array<String>): Int {
-        
+        val stack = ArrayDeque<Int>()
+
+        for (token in tokens) {
+            if (isOperator(token)) {
+                val b = stack.removeLast()
+                val a = stack.removeLast()
+                stack.addLast(applyOperator(token, a, b))
+            } else {
+                stack.addLast(token.toInt())
+            }
+        }
+
+        return stack.removeLast()
+    }
+
+    private fun isOperator(operator: String): Boolean {
+        return operator in listOf("+", "-", "/", "*")
+    }
+
+    private fun applyOperator(operator: String, a: Int, b: Int): Int {
+        return when (operator) {
+            "+" -> a + b
+            "-" -> a - b
+            "*" -> a * b
+            "/" -> a / b
+            else -> -1
+        }
     }
 }
