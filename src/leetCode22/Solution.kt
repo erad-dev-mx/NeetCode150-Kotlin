658ef70002d3e44887823218fa3d6465cde9ec36
erad-dev-mx <erick_rr1@outlook.com>
2025-07-04 10:29:02 -0600

    LeetCode 22 Solution

---
+++ src/leetCode22/Solution.kt
@@ -0,0 +1,29 @@
+package leetCode22
+
+class Solution {
+    fun generateParenthesis(n: Int): List<String> {
+        val ans = mutableListOf<String>()
+        backtrack(ans, StringBuilder(), 0, 0, n)
+
+        return ans
+    }
+
+    fun backtrack(ans: MutableList<String>, cur: StringBuilder, open: Int, close: Int, max: Int) {
+        if (cur.length == max * 2) {
+            ans.add(cur.toString())
+            return
+        }
+
+        if (open < max) {
+            cur.append("(")
+            backtrack(ans, cur, open + 1, close, max)
+            cur.deleteCharAt(cur.length - 1)
+        }
+
+        if (close < open) {
+            cur.append(")")
+            backtrack(ans, cur, open, close + 1, max)
+            cur.deleteCharAt(cur.length - 1)
+        }
+    }
+}
