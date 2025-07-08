12b70f721bef8c02bd3c351f8e3f25e15d8a864d
erad-dev-mx <erick_rr1@outlook.com>
2025-07-07 19:48:17 -0600

    LeetCode 739 test cases

--- src/leetCode739/Solution.kt
+++ src/leetCode739/Solution.kt
@@ -23,3 +23,23 @@ class Solution {
         return answer
     }
 }
+
+// Test cases
+fun main() {
+    val solution = Solution()
+
+    val temps1 = intArrayOf(73, 74, 75, 71, 69, 72, 76, 73)
+    println("Output: ${solution.dailyTemperatures(temps1).joinToString()}")
+
+    val temps2 = intArrayOf(30, 40, 50, 60)
+    println("Output: ${solution.dailyTemperatures(temps2).joinToString()}")
+
+    val temps3 = intArrayOf(30, 60, 90)
+    println("Output: ${solution.dailyTemperatures(temps3).joinToString()}")
+
+    val temps4 = intArrayOf(90, 80, 70, 60, 50)
+    println("Output: ${solution.dailyTemperatures(temps4).joinToString()}")
+
+    val temps5 = intArrayOf(70, 71, 70, 71, 70)
+    println("Output: ${solution.dailyTemperatures(temps5).joinToString()}")
+}
