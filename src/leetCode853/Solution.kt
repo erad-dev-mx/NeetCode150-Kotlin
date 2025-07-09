e5f60dde70bd92271520a95db2852837b3f1028a
erad-dev-mx <erick_rr1@outlook.com>
2025-07-08 20:11:26 -0600

    LeetCode 853 solution

---
+++ src/leetCode853/Solution.kt
@@ -0,0 +1,26 @@
+package leetCode853
+
+class Solution {
+    fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
+        val n = position.size
+        val cars = Array(n) { DoubleArray(2) }
+
+        for (i in 0 until n) {
+            cars[i][0] = position[i].toDouble() // Car position
+            cars[i][1] = (target - position[i]).toDouble() / speed[i] // Time to reach to the end
+        }
+
+        cars.sortByDescending { it[0] }
+
+        var count = 0
+        var prevTime = 0.0
+        for (car in cars) {
+            if (car[1] > prevTime) {
+                count++
+                prevTime = car[1]
+            }
+        }
+
+        return count
+    }
+}
