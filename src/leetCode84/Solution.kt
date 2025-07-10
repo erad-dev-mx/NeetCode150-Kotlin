1b93c9b575ae4d9354bbc344f1305a78a64d6f85
erad-dev-mx <erick_rr1@outlook.com>
2025-07-09 20:02:17 -0600

    LeetCode 84 test cases

--- src/leetCode84/Solution.kt
+++ src/leetCode84/Solution.kt
@@ -25,3 +25,23 @@ class Solution {
         return maxArea
     }
 }
+
+// Test cases
+fun main() {
+    val solution = Solution()
+
+    val h1 = intArrayOf(2, 1, 5, 6, 2, 3)
+    println("Max area: ${solution.largestRectangleArea(h1)}")
+
+    val h2 = intArrayOf(2, 4)
+    println("Max area: ${solution.largestRectangleArea(h2)}")
+
+    val h3 = intArrayOf(6, 2, 5, 4, 5, 1, 6)
+    println("Max area: ${solution.largestRectangleArea(h3)}")
+
+    val h4 = intArrayOf(1, 2, 3, 4, 5)
+    println("Max area: ${solution.largestRectangleArea(h4)}")
+
+    val h5 = intArrayOf(5, 4, 3, 2, 1)
+    println("Max area: ${solution.largestRectangleArea(h5)}")
+}
