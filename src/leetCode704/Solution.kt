21212cd64c7c1f6a4e605460e7747aace0db017d
erad-dev-mx <erick_rr1@outlook.com>
2025-07-10 20:43:03 -0600

    LeetCode 704 test cases

--- src/leetCode704/Solution.kt
+++ src/leetCode704/Solution.kt
@@ -19,3 +19,23 @@ class Solution {
         return -1
     }
 }
+
+// Test cases
+fun main() {
+    val solution = Solution()
+
+    val nums1 = intArrayOf(-1, 0, 3, 5, 9, 12)
+    println("Index: ${solution.search(nums1, 9)}")
+
+    val nums2 = intArrayOf(-1, 0, 3, 5, 9, 12)
+    println("Index: ${solution.search(nums2, 2)}")
+
+    val nums3 = intArrayOf(1, 3, 5, 7, 9)
+    println("Index: ${solution.search(nums3, 1)}")
+
+    val nums4 = intArrayOf(1, 3, 5, 7, 9)
+    println("Index: ${solution.search(nums4, 9)}")
+
+    val nums5 = intArrayOf(1)
+    println("Index: ${solution.search(nums5, 1)}")
+}
