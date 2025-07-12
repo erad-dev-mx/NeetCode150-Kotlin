b2c2b43cd1d29d288b707236bb242773f7b4332c
erad-dev-mx <erick_rr1@outlook.com>
2025-07-11 22:30:24 -0600

    LeetCode 74 init & solution

---
+++ src/leetCode74/Solution.kt
@@ -0,0 +1,26 @@
+package leetCode74
+
+class Solution {
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        val m = matrix.size // rows
+        val n = matrix[0].size // cols
+
+        var left = 0
+        var right = m * n - 1
+
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            val midVal = matrix[mid / n][mid % n] // Convert 1D index to 2D coordinates: row = mid / n, column = mid % n
+
+            if (midVal == target) {
+                return true
+            } else if (midVal < target) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+
+        return false
+    }
+}
