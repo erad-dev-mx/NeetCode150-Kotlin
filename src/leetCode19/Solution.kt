e4df58adebb311832eb83676e4fcb0e7f4e1346e
erad-dev-mx <erick_rr1@outlook.com>
2025-07-23 19:57:41 -0600

    LeetCode 19 comments

--- src/leetCode19/Solution.kt
+++ src/leetCode19/Solution.kt
@@ -1,5 +1,12 @@
 package leetCode19
 
+/**
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * */
+
+
 class Solution {
     fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
         val dummy = ListNode(1)
