d277e9cfa0fc8086cc084c19ee34bf7114b9e44a
erad-dev-mx <erick_rr1@outlook.com>
2025-07-23 19:57:06 -0600

    LeetCode 19 solution

---
+++ src/leetCode19/Solution.kt
@@ -0,0 +1,36 @@
+package leetCode19
+
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        val dummy = ListNode(1)
+        dummy.next = head;
+
+        var front: ListNode? = dummy;
+        var back: ListNode? = dummy;
+
+        for (i in 0..n) {
+            front = front?.next
+        }
+
+        while (front != null) {
+            front = front.next
+            back = back?.next
+        }
+        back?.next = back.next?.next
+
+        return dummy.next;
+    }
+}
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
