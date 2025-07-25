be71ce45ebfd2d802ebad566d11998a57d69abe4
erad-dev-mx <erick_rr1@outlook.com>
2025-07-24 21:03:49 -0600

    LeetCode 138 solution

---
+++ src/leetCode138/Solution.kt
@@ -0,0 +1,36 @@
+package leetCode138
+
+class Solution {
+    val visited = HashMap<Node, Node>()
+
+    fun copyRandomList(node: Node?): Node? {
+        if (node == null) return null
+
+        if (visited.containsKey(node)) {
+            return visited[node]
+        }
+
+        val newNode = Node(node.`val`)
+        visited[node] = newNode
+
+        newNode.next = copyRandomList(node.next)
+        newNode.random = copyRandomList(node.random)
+
+        return newNode
+    }
+}
+
+/**
+ * Example:
+ * var ti = Node(5)
+ * var v = ti.`val`
+ * Definition for a Node.
+ * class Node(var `val`: Int) {
+ *     var next: Node? = null
+ *     var random: Node? = null
+ * }
+ */
+class Node(var `val`: Int) {
+    var next: Node? = null
+    var random: Node? = null
+}
