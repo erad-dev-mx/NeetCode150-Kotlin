662a603dd85a88577e3eb91a4688aae3fc5df91d
erad-dev-mx <erick_rr1@outlook.com>
2025-07-29 19:28:11 -0600

    LeetCode 141 init

---
+++ src/leetCode141/Solution.kt
@@ -0,0 +1,21 @@
+package leetCode141
+
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+
+    }
+}
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
