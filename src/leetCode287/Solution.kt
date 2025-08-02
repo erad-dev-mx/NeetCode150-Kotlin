d2dc627cfbaf7c237e0580f8c04706b2dec94312
erad-dev-mx <erick_rr1@outlook.com>
2025-08-01 20:52:34 -0600

    LeetCode 287 test cases

--- src/leetCode287/Solution.kt
+++ src/leetCode287/Solution.kt
@@ -21,3 +21,23 @@ class Solution {
         return fast
     }
 }
+
+// Test cases
+fun main() {
+    val solution = Solution()
+
+    val nums1 = intArrayOf(1, 3, 4, 2, 2)
+    println("Duplicate: ${solution.findDuplicate(nums1)}")
+
+    val nums2 = intArrayOf(3, 1, 3, 4, 2)
+    println("Duplicate: ${solution.findDuplicate(nums2)}")
+
+    val nums3 = intArrayOf(1, 1)
+    println("Duplicate: ${solution.findDuplicate(nums3)}")
+
+    val nums4 = intArrayOf(1, 1, 2)
+    println("Duplicate: ${solution.findDuplicate(nums4)}")
+
+    val nums5 = intArrayOf(3, 4, 3, 2, 1, 5, 6)
+    println("Duplicate: ${solution.findDuplicate(nums5)}")
+}
