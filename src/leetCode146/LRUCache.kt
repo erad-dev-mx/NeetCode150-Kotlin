31cc1affa7a485a8abb558ae3dba5a000fc9efe5
erad-dev-mx <erick_rr1@outlook.com>
2025-08-05 18:50:27 -0600

    LeetCode146 solution

--- src/leetCode146/LRUCache.kt
+++ src/leetCode146/LRUCache.kt
@@ -2,14 +2,58 @@ package leetCode146
 
 class LRUCache(capacity: Int) {
 
+    private inner class Node(var key: Int, var value: Int) {
+        var prev: Node? = null
+        var next: Node? = null
+    }
+
+    private val capacity = capacity
+    private val map = HashMap<Int, Node>()
+    private val head = Node(0, 0)
+    private val tail = Node(0, 0)
+
+    init {
+        head.next = tail
+        tail.prev = head
+    }
+
     fun get(key: Int): Int {
-        
+        val node = map[key] ?: return -1
+        remove(node)
+        insertAtHead(node)
+        return node.value
     }
 
     fun put(key: Int, value: Int) {
-        
+        if (map.containsKey(key)) {
+            val node = map[key]!!
+            node.value = value
+            remove(node)
+            insertAtHead(node)
+        } else {
+            if (map.size == capacity) {
+                val toRemove = tail.prev!!
+                map.remove(toRemove.key)
+                remove(toRemove)
+            }
+            val newNode = Node(key, value)
+            map[key] = newNode
+            insertAtHead(newNode)
+        }
     }
 
+    // Util functions
+    private fun remove(node: Node) {
+        node.prev?.next = node.next
+        node.next?.prev = node.prev
+    }
+
+    private fun insertAtHead(node: Node) {
+        node.next = head.next
+        head.next?.prev = node
+        head.next = node
+        node.prev = head
+    }
 }
 
 /**
