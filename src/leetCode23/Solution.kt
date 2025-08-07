1962193f963cd075ae909436c3124fdbb0728426
erad-dev-mx <erick_rr1@outlook.com>
2025-08-06 19:09:45 -0600

    LeetCode 23 solution

--- src/leetCode23/Solution.kt
+++ src/leetCode23/Solution.kt
@@ -4,7 +4,24 @@ import java.util.PriorityQueue
 
 class Solution {
     fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+        val minHeap = PriorityQueue<Int>()
 
+        for (list in lists) {
+            var node = list
+            while (list != null) {
+                minHeap.add(node?.`val`)
+                node = node?.next
+            }
+        }
+
+        val dummy = ListNode(1)
+        var merge = dummy
+        while (minHeap.isNotEmpty()) {
+            merge.next = ListNode(minHeap.remove())
+            merge = merge.next!!
+        }
+
+        return dummy.next
     }
 }
 
