9f65958939ef296b4c16452d4ffb2746699046e6
erad-dev-mx <erick_rr1@outlook.com>
2025-08-09 22:43:36 -0600

    LeetCode 25 test cases

--- src/leetCode226/Solution.kt
+++ src/leetCode226/Solution.kt
@@ -1,5 +1,8 @@
 package leetCode226
 
+import java.util.LinkedList
+import java.util.Queue
+
 class Solution {
     fun invertTree(root: TreeNode?): TreeNode? {
         if (root == null) return null
@@ -23,4 +26,32 @@ class Solution {
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+}
+
+// Test cases
+fun main() {
+    val root = TreeNode(4)
+    root.left = TreeNode(2)
+    root.right = TreeNode(7)
+    root.left?.left = TreeNode(1)
+    root.left?.right = TreeNode(3)
+    root.right?.left = TreeNode(6)
+    root.right?.right = TreeNode(9)
+
+    val solution = Solution()
+    val inverted = solution.invertTree(root)
+
+    printLevelOrder(inverted) // 4 7 2 9 6 3 1
+}
+
+fun printLevelOrder(root: TreeNode?) {
+    if (root == null) return
+    val queue: Queue<TreeNode> = LinkedList()
+    queue.add(root)
+    while (queue.isNotEmpty()) {
+        val node = queue.poll()
+        print("${node.`val`} ")
+        node.left?.let { queue.add(it) }
+        node.right?.let { queue.add(it) }
+    }
 }
