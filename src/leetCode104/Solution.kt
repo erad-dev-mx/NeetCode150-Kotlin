2ac3bd90c31b9f0f28df4a72d46d3dd43ea8de78
erad-dev-mx <erick_rr1@outlook.com>
2025-08-11 19:54:22 -0600

    LeetCode 104 init

---
+++ src/leetCode104/Solution.kt
@@ -0,0 +1,18 @@
+package leetCode104
+
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+
+    }
+}
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ */
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
