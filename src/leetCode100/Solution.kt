00b4034e6e20e52a619164919b48fd6ccc98cc84
erad-dev-mx <erick_rr1@outlook.com>
2025-08-14 20:56:33 -0600

    LeetCode 100 definition for binary tree

--- src/leetCode100/Solution.kt
+++ src/leetCode100/Solution.kt
@@ -1,8 +1,18 @@
 package leetCode100
 
-
 class Solution {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
-        
+
     }
+}
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ */
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
 }
