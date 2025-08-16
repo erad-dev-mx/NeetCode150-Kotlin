182a00ba02dd7b46b88ae145d88a6cf6bff17361
erad-dev-mx <erick_rr1@outlook.com>
2025-08-15 20:00:51 -0600

    LeetCode 199 test cases

--- src/leetCode199/Solution.kt
+++ src/leetCode199/Solution.kt
@@ -41,3 +41,19 @@ class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
 }
+
+// Test cases
+fun main() {
+    val root1 = TreeNode(1)
+    root1.left = TreeNode(2)
+    root1.right = TreeNode(3)
+    root1.left?.right = TreeNode(5)
+    root1.right?.right = TreeNode(4)
+
+    val root2 = TreeNode(1)
+    root2.left = TreeNode(2)
+
+    val solution = Solution()
+    println(solution.rightSideView(root1)) // Expected: [1, 3, 4]
+    println(solution.rightSideView(root2)) // Expected: [1, 2]
+}
