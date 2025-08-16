6f40ae20ce5b72122cdfe9db189980d94fcf2b09
erad-dev-mx <erick_rr1@outlook.com>
2025-08-15 19:53:16 -0600

    LeetCode 199 init

---
+++ src/leetCode199/Solution.kt
@@ -0,0 +1,18 @@
+package leetCode199
+
+class Solution {
+    fun rightSideView(root: TreeNode?): List<Int> {
+
+    }
+}
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ */
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
